b366db2f42dc2ea2244828caade13b24f3de0593
Kimberley Bezuidenhout <kimberleybezuidenhout@gmail.com>
2025-11-08 18:21:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3536671082-6b52d",
+  "appId": "1:1089361294341:web:2ed96f4950427aaaae186c",
+  "apiKey": "AIzaSyB313cahgsdrWYyeBzlVl9yb97QqaS8PPI",
+  "authDomain": "studio-3536671082-6b52d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1089361294341"
+};
