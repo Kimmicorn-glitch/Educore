1061327cb9daa4efdb345c18515d8a0eb884b862
Kimberley Bezuidenhout <kimberleybezuidenhout@gmail.com>
2025-11-09 10:34:01 +0000

    A tree hydrated but some attributes of the server rendered HTML didn't m

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,7 @@
 
+'use client';
+
+import { useState, useEffect } from 'react';
 import Header from "@/components/layout/header";
 import { MainSidebar } from "@/components/layout/main-sidebar";
 import OnboardingTutorial from "@/components/tutorial/onboarding-tutorial";
@@ -14,6 +17,32 @@ export default function AppLayout({
 }: {
     children: React.ReactNode;
 }) {
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+
+    if (!mounted) {
+        return (
+             <FirebaseClientProvider>
+                <SidebarProvider>
+                    <Sidebar>
+                        <MainSidebar />
+                    </Sidebar>
+                    <SidebarInset>
+                        <div className="flex h-full flex-col">
+                            <Header />
+                            <main className="flex-1 overflow-y-auto p-4 pt-6 md:p-8">
+                                {/* You can add a skeleton loader here if you want */}
+                            </main>
+                        </div>
+                    </SidebarInset>
+                </SidebarProvider>
+            </FirebaseClientProvider>
+        )
+    }
+    
     return (
         <FirebaseClientProvider>
             <SidebarProvider>
